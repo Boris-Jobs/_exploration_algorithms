fb13b8f4c13391e109d5c29b70d866356a77e56c
boris sigma <1322553126@qq.com>
2024-05-09 23:29:01 +0800

    updated

--- src/SLList.java
+++ src/SLList.java
@@ -6,6 +6,19 @@ import static java.lang.System.*;
  */
 
 public class SLList {
+
+    /* This is a nested class. */
+    /** Declaring a nested class as static means that methods inside the static class can not access any of the members of the enclosing class. In this case, it means that no method in IntNode would be able to access first, addFirst, or getFirst.
+     * Put another way, if you examine the code here, you'll see that the IntNode class never uses the first variable of SLList, nor any of SLList's methods. As a result, we can use the static keyword, which means the IntNode class doesn't get a reference to its boss, saving us a small amount of memory.*/
+    public static class IntNode {
+        public int item;
+        public IntNode next;
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
     private IntNode first;
 
     public SLList(int x) {

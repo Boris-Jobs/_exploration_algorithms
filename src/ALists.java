088c602c3e78b800fab788c368076bf87c97f931
boris sigma <1322553126@qq.com>
2024-06-08 23:45:53 +0800

    Create ALists.java

---
+++ src/ALists.java
@@ -0,0 +1,19 @@
+/**
+ * Our Mission: Understand the Universe
+ * Created by borisσ (FrameX Inc.) on 2024/6/8 23:28.
+ *
+ * The Law of the Broken Futon
+ *
+ * You might ask yourself why we spent so much time and space covering what seems like a triviality. This is probably especially true if you have prior Java experience. The reason is that it is very easy for a student to have a half-cocked understanding of this issue, allowing them to write code, but without true comprehension of what's going on.
+ *
+ * While this might be fine in the short term, in the long term, doing problems without full understanding may doom you to failure later down the line.
+ */
+ 
+public class ALists {
+    // array-based list & linked-list
+    // Why does accessing an array element take constant time?
+    // https://www.quora.com/Why-does-accessing-an-array-element-take-constant-time
+    public static void main(String[] args) {
+
+    }
+}

808edda46ced537e7a10f10950fd51c73eb5e6dd
boris sigma <1322553126@qq.com>
2024-06-22 19:12:28 +0800

    tested the interface in Java

---
+++ src/_test.java
@@ -0,0 +1,37 @@
+/**
+ * Our Mission: Understand the Universe
+ * Created by borisσ (FrameX Inc.) on 2024/6/21 10:49.
+ *
+ * The Law of the Broken Futon
+ *
+ * You might ask yourself why we spent so much time and space covering what seems like a triviality. This is probably especially true if you have prior Java experience. The reason is that it is very easy for a student to have a half-cocked understanding of this issue, allowing them to write code, but without true comprehension of what's going on.
+ *
+ * While this might be fine in the short term, in the long term, doing problems without full understanding may doom you to failure later down the line.
+ */
+ 
+public class _test implements _test_interface {
+    @Override
+    public void compute() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
